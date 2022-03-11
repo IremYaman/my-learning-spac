686f6f1e3145967386a95be6354e909cb47fa483
IremYaman <100487679+IremYaman@users.noreply.github.com>
2022-03-11 20:26:30 +0300

    Add files via upload

---
+++ data-structures/list-methods-demo.py
@@ -0,0 +1,35 @@
+names = ["Ali", "Yağmur", "Hakan", "Deniz"]
+years = [1998, 2000, 1998, 1987]
+
+# names.append("Cenk")
+# names.insert(0, "Sena")
+# names.remove("Deniz")
+# index = names.index("Deniz")
+# print(index)
+# result = "Ali" in names
+# names.reverse()                                 # (names = ) veya (result = ) şeklinde yazınca olmadı 
+# print(names)
+# names.sort()
+# print(names)
+# years.sort()
+# print(years)
+# str = "Chevrolet, Dacia".split(",")
+# print(str)
+# val = max(years)
+# val = min(years)
+# print(val)
+# print(years.count(1998))
+# years.clear()
+# print(years)
+markalar = []
+marka = input("marka: ")
+markalar.append(marka)
+
+marka = input("marka: ")
+markalar.append(marka)
+
+marka = input("marka: ")
+markalar.append(marka)
+
+
+print(markalar)
---
+++ data-structures/lists_demo.py
@@ -0,0 +1,28 @@
+
+cars  = ["BMW", "Mercedes", "Opel", "Mazda"]
+# print(len(cars))
+# print(cars[0])
+# print(cars[3])
+# print(cars[-1])
+# cars[-1] = "Toyota"
+# print(cars)
+# result = "Mercedes" in cars                 # in komutu
+# print(result)
+# print(cars[-2])
+# print(cars[0:3])
+# cars[-2:] = "Toyota", "Renault"
+# print(cars)
+# cars2 = ["Audi", "Nissan"]
+# print(cars + cars2)
+# del cars[-1]                                 # del komutu
+# print(cars)                     
+
+# print(cars[::-1])
+
+studentA = ["Yiğit", " Bilgi", 2010, [70, 60, 70]]
+studentB = ["Sena", "Turan", 1999, [80, 80, 70]]
+studentC = ["Ahmet", "Turan", 1998, [80, 70, 90]]
+
+print(studentA)
+print(studentB)
+print(studentC)
