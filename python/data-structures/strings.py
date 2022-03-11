daa56f3d19a4fc72a806b45b9a3958454fff5384
IremYaman <100487679+IremYaman@users.noreply.github.com>
2022-03-11 20:23:08 +0300

    Add files via upload

---
+++ python/data-structures/strings.py
@@ -0,0 +1,13 @@
+name = "Irem "
+surname = "Yaman"
+age = 19
+introducing = "Hello, My name is " + name + surname + "\nand i am " + str(age) + " years old"
+# print(introducing)
+
+# print(introducing[4])
+
+# lenght = len(introducing)
+
+# print(introducing [lenght-1])
+# print (introducing[2:8])
+# print(introducing[4:])
