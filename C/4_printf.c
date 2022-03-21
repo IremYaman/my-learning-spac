adf465b617d8a949f643fbcf49aace8456144555
IremYaman <100487679+IremYaman@users.noreply.github.com>
2022-03-21 18:49:15 +0300

    Add files via upload

---
+++ C/4_printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	/* 
+	%c => single character
+	%f => float
+	%s => string
+	%d => integer
+	*/
+	printf("My favorite %s is %d.", "number",1);
+	
+	return 0;
+}
---
+++ C/5_working-with-numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*
+int main()
+{
+	printf("%d\n",5);     // 5 
+	printf("%d\n",5/4);   // 1 
+	printf("%f\n",5/4);   // 0.0 bc we are working with ints and asking for printing it as a floating number. it is not possible. 
+	printf("%f\n",5/4.0); // 1.25 
+	
+	
+	return 0;
+}
+*/
+
+int main()
+{
+	printf("%f\n", pow(2,3));    //üs alma fonksiyonu
+	printf("%f\n", sqrt(36));    //hangi sayýnýn karesi olduðunu bulma fonksiyonu
+	printf("%f\n", ceil(32.4));  //sayýyý üste yuvarlama fonksiyonu
+	printf("%f\n", floor(32.8)); //sayýyý alta yuvarlama fonksiyonu
+
+
+	
+	return 0;
+}
+
+
